31a7784e10875c1d3ffc99d0f0d01120dc6394dd
ReenExe <ipourri@gmail.com>
2015-12-18 02:27:20 +0200

    copy-past from https://github.com/ReenExe/SymfonyGoodFiltration

---
+++ skill/scraping.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+startTime=`date +%s`
+
+STEPS=(
+    "scrap:skill:site:list"
+    "scrap:skill:site:page"
+)
+
+for STEP in "${STEPS[@]}"
+do
+    while true; do
+        bin/console $STEP
+        EXIT_CODE=$?
+        if test $EXIT_CODE -eq 1
+        then
+            echo "Done"
+            break
+        fi
+        echo '...';
+    done
+done
+
+endTime=`date +%s`
+echo execution time was `expr $endTime - $startTime` s.
