15d338691d7196b9ddb489237fe6663504ef1761
ReenExe <ipourri@gmail.com>
2016-03-02 21:20:14 +0200

    move to common

---
+++ src/AppBundle/Service/Scrap/ListContentAnalyzer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Service\Scrap;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+class ListContentAnalyzer implements ListContentAnalyzerInterface
+{
+    /**
+     * @var string
+     */
+    protected $firstPage;
+
+    /**
+     * @var string
+     */
+    protected $nextPageSelector;
+
+    /**
+     * @var string
+     */
+    protected $profileLinkSelector;
+
+    /**
+     * ListContentAnalyzer constructor.
+     * @param $firstPage
+     * @param $nextPageSelector
+     * @param $profileLinkSelector
+     */
+    public function __construct($firstPage, $nextPageSelector, $profileLinkSelector)
+    {
+        $this->firstPage = $firstPage;
+        $this->nextPageSelector = $nextPageSelector;
+        $this->profileLinkSelector = $profileLinkSelector;
+    }
+
+    public function getFirstPage()
+    {
+        return $this->firstPage;
+    }
+
+    public function getNextPage(Crawler $crawler)
+    {
+        $nextLinkCrawler = $crawler->filter($this->nextPageSelector);
+
+        return $nextLinkCrawler->count() ? $nextLinkCrawler->attr('href') : false;
+    }
+
+    public function getPageLinkCollection(Crawler $crawler)
+    {
+        return $crawler
+            ->filter($this->profileLinkSelector)
+            ->each(function (Crawler $crawler) {
+                return $crawler->attr('href');
+            });
+    }
+}
