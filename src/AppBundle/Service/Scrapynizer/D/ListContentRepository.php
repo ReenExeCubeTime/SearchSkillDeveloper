1861cd3b7ecac5d9e4328ca8fc76d6e319b9659c
ReenExe <ipourri@gmail.com>
2016-05-21 20:54:07 +0300

    progress

--- src/AppBundle/Service/Scrapynizer/D/ListContentRepository.php
+++ src/AppBundle/Service/Scrapynizer/D/ListContentRepository.php
@@ -3,24 +3,55 @@
 namespace AppBundle\Service\Scrapynizer\D;
 
 use ReenExe\Scrapynizer\Repository\ListContentRepositoryInterface;
+use Doctrine\DBAL\Connection;
 
 class ListContentRepository implements ListContentRepositoryInterface
 {
     /**
-     * @return string
+     * @var Connection
      */
-    public function getLast()
-    {
-        // TODO: Implement getLast() method.
-    }
+    private $connection;
 
     /**
-     * @param $path
-     * @param $value
-     * @return mixed
+     * @var string
      */
+    private $table;
+
+    public function __construct(Connection $connection, $table)
+    {
+        $this->connection = $connection;
+        $this->table = $table;
+    }
+
+    public function getLast()
+    {
+        $this->create();
+
+        return $this->connection->executeQuery("
+                SELECT `value` FROM `$this->table`
+                ORDER BY `id` DESC
+                LIMIT 1;
+            ")
+            ->fetchColumn();
+    }
+
     public function save($path, $value)
     {
-        // TODO: Implement save() method.
+        $this->connection->insert($this->table, [
+            'path' => $path,
+            'value' => $value,
+        ]);
+    }
+
+    private function create()
+    {
+        $this->connection->exec("
+            CREATE TABLE IF NOT EXISTS `$this->table` (
+                `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+                `path` VARCHAR(255),
+                `value` MEDIUMBLOB,
+                UNIQUE KEY (`path`)
+            );
+        ");
     }
 }
--- src/AppBundle/Service/Scrapynizer/QueueCommandService.php
+++ src/AppBundle/Service/Scrapynizer/QueueCommandService.php
@@ -2,10 +2,22 @@
 
 namespace AppBundle\Service\Scrapynizer;
 
+use ReenExe\Scrapynizer\Scraper\ListScraperInterface;
+
 class QueueCommandService implements QueueCommandServiceInterface
 {
+    /**
+     * @var ListScraperInterface
+     */
+    private $service;
+
+    public function __construct(ListScraperInterface $service)
+    {
+        $this->service = $service;
+    }
+
     public function execute($limit)
     {
-
+        return $this->service->process($limit);
     }
 }
