d56ae7139742a3e04baf1245044f741c4e36cde7
ReenExe <ipourri@gmail.com>
2016-05-21 19:53:55 +0300

    default structure

---
+++ src/AppBundle/Service/Scrapynizer/D/ListContentRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Service\Scrapynizer\D;
+
+use ReenExe\Scrapynizer\Repository\ListContentRepositoryInterface;
+
+class ListContentRepository implements ListContentRepositoryInterface
+{
+    /**
+     * @return string
+     */
+    public function getLast()
+    {
+        // TODO: Implement getLast() method.
+    }
+
+    /**
+     * @param $path
+     * @param $value
+     * @return mixed
+     */
+    public function save($path, $value)
+    {
+        // TODO: Implement save() method.
+    }
+}
