dbe728e3226a65cf84a66a4cede78f4e73062dca
ReenExe <ipourri@gmail.com>
2016-12-29 02:34:26 +0200

    available cities

--- src/AppBundle/Command/D/AnalyzeCommand.php
+++ src/AppBundle/Command/D/AnalyzeCommand.php
@@ -5,6 +5,7 @@ namespace AppBundle\Command\D;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\DBAL\Connection;
 
 class AnalyzeCommand extends ContainerAwareCommand
 {
@@ -20,6 +21,37 @@ class AnalyzeCommand extends ContainerAwareCommand
         $duration = microtime(true) - $startTime;
         $memory = memory_get_usage(true);
         $peak = memory_get_peak_usage(true);
+
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        $cities = <<<'SQL'
+            DROP TABLE IF EXISTS `city`;
+            CREATE TABLE `city` (
+                `name` VARCHAR(255)
+            );
+            INSERT INTO `city` (`name`) VALUES
+            ('Киев'),
+            ('Харьков'),
+            ('Львов'),
+            ('Днепропетровск'),
+            ('Одесса'),
+            ('Украина'),
+            ('Винница'),
+            ('Николаев'),
+            ('Запорожье'),
+            ('Хмельницкий'),
+            ('Ивано-Франковск'),
+            ('Черкассы'),
+            ('Черновцы'),
+            ('Житомир'),
+            ('Чернигов'),
+            ('Симферополь'),
+            ('Донецк'),
+            ('Севастополь');
+SQL;
+
+        $connection->exec($cities);
 
         $output->writeln([
             "<info>Execute:  $duration</info>",
